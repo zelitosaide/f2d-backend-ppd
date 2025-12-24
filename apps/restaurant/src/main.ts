19b7d053e3ba2e1167974ea531e9b08d5690590e
zelitosaide <zelitosaide83@gmail.com>
2025-12-24 22:52:19 +0200

    Cors

--- apps/restaurant/src/main.ts
+++ apps/restaurant/src/main.ts
@@ -3,11 +3,11 @@ import { RestaurantModule } from "./restaurant.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(RestaurantModule);
-  app.enableCors({
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  });
+  // app.enableCors({
+  //   origin: "*",
+  //   methods: ["GET", "POST", "PATCH", "DELETE"],
+  //   allowedHeaders: ["Content-Type", "Authorization"],
+  // });
   await app.listen(process.env.port ?? 3003);
 }
 bootstrap();
